8c35b0b97404b30de3359433e1ea5e05f5847994
Emiliano Hotes <emiliano.hotes@despegar.com>
2023-05-11 09:35:35 -0300

    feat: changed remove method implementation

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -72,12 +72,11 @@ export class PokemonService {
   }
 
   async remove(id: string) {
-    // const pokemon = await this.findOne(id);
-    // await pokemon.deleteOne();
-    // return { id };
+    const { deletedCount } = await this.pokemonModel.deleteOne({ _id: id });
+    if (deletedCount === 0)
+      throw new BadRequestException(`Pokemon with id "${id}" not found`);
 
-    const result = this.pokemonModel.findByIdAndDelete(id);
-    return result;
+    return;
   }
 
   private handleExceptions(error: any) {
