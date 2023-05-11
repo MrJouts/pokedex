9a1e901d2e1bfeae86e6311804ab92bd455553aa
Emiliano Hotes <emiliano.hotes@despegar.com>
2023-05-11 10:27:32 -0300

    feat: added pokemons to database from seed

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,22 +1,32 @@
 import { Injectable } from '@nestjs/common';
 import axios, { AxiosInstance } from 'axios';
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
+
+import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
 import { PokeResponse } from './interfaces/poke-response.interface';
 
 @Injectable()
 export class SeedService {
+  constructor(
+    @InjectModel(Pokemon.name)
+    private readonly pokemonModel: Model<Pokemon>,
+  ) {}
+
   private readonly axios: AxiosInstance = axios;
 
   async execureSeed() {
     const { data } = await this.axios.get<PokeResponse>(
-      'https://pokeapi.co/api/v2/pokemon?limit=10',
+      'https://pokeapi.co/api/v2/pokemon?limit=5',
     );
 
-    data.results.forEach(({ name, url }) => {
+    data.results.forEach(async ({ name, url }) => {
       const segments = url.split('/');
       const no = +segments[segments.length - 2];
-      console.log({ name, no });
+
+      const pokemon = await this.pokemonModel.create({ name, no });
     });
 
-    return data.results;
+    return 'Seed Executed';
   }
 }
